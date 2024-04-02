a3e35e05a428fec58de33b071637d124b497b32e
Jonathan Buckel <jonathanbuckel38@gmail.com>
2024-04-02 15:26:43 -0400

    Created load and save functions.

--- Utils.py
+++ Utils.py
@@ -1,9 +1,28 @@
+import json
 # Load file
+# Accepts a path
+# Returns a dictionary
+def load(path):
+    f = open(path, "r")
+    data = json.load(f)
+    f.close()
+    return json.loads(data)
 
 # Save file
+# Accepts a path and dictionary
+def save(path, data):
+    json_object = json.dumps(data, indent=4)
+    f = open(path, "w")
+    f.write(json_object)
+    f.close()
 
 # Is in row
+# Accepts 2 dimensional array and row number
+def in_row(grid, row):
+    return false
 
 # Is in column
+# Accepts 2 dimensional array and column number
+def in_column(grid, column):
+    return false
 
-# Is in block/set/etc.
